27d9f032effaa87a975903a4fcadda2d76db10cc
Eduardo Hernández Ley <ehernandezl@concredito.com.mx>
2023-05-23 13:49:16 -0700

    Explore

--- init.lua
+++ init.lua
@@ -38,8 +38,6 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
